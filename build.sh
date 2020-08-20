9c239e1e082853bc27a677124c0ee01e4e48b22c
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-08-20 20:25:56 +0100

    fix file removal

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+rm alternatives.txt
 set -e
 
 env CGO_ENABLED=0 GOOS=linux go build -o deployer .
 docker build -t brunoanjos/deployer:latest .
-rm alternatives.txt
 touch alternatives.txt
