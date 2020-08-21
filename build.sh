9f0b02aa1844258bf95ba2d3aa95f82e4d5b33d1
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-08-21 11:00:23 +0100

    delete dir content instead of dir itself

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-rm -rf alternatives/
+rm -rf alternatives/*
 set -e
 
 env CGO_ENABLED=0 GOOS=linux go build -o deployer .
 docker build -t brunoanjos/deployer:latest .
-mkdir alternatives/
